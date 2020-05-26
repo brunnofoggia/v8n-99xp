82944ad1da68246e1447b3170b0b047a3c29988a
Bruno Foggia <bruno@vixs.com.br>
2020-05-26 11:28:49 -0300

    fix on password match

--- src/v8n-99xp.js
+++ src/v8n-99xp.js
@@ -82,7 +82,7 @@ v8n.extend({
     passwordMatch(expected) {
         return (value, attrs) => {
             var validate = function (data) {
-                if (_typeof(data) !== 'object' || typeof data[0] === 'undefined') {
+                if (typeof data !== 'object' || typeof data[0] === 'undefined') {
                     return true;
                 }
 
